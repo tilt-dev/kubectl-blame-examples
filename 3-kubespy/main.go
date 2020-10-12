fae8cc2b8b27e12b388f65305c900521c667f351
Nick Santos <nicholas.j.santos@gmail.com>
2020-10-12 00:16:51 -0400

    add kubespy (#8)

---
+++ 3-kubespy/main.go
@@ -0,0 +1,301 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/tilt-dev/kubecon-2020-talk/3-kubespy/kubespy"
+	"github.com/tilt-dev/localregistry-go"
+	"github.com/tjarratt/babble"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	yamlEncoder "sigs.k8s.io/yaml"
+)
+
+var alphaRegexp = regexp.MustCompile("[^a-zA-Z-]")
+
+func main() {
+	var seed int64
+	var contentName string
+	var crash bool
+	flag.Int64Var(&seed, "seed", time.Now().UnixNano(), "Seed the random label generator")
+	flag.StringVar(&contentName, "contents", "", "Contents of index.html. Defaults to the random label")
+	flag.BoolVar(&crash, "crash", false, "When set, replaces the entrypoint on the container so it crashes")
+	flag.Parse()
+	rand.Seed(seed)
+
+	// Generate a random label for this deployment
+	id := sanitize(babble.NewBabbler().Babble())
+	if contentName == "" {
+		contentName = id
+	}
+	contents := fmt.Sprintf("Hello world! I'm deployment %s!", contentName)
+	imageTag := fmt.Sprintf("deploy-%x", md5.Sum([]byte(contentName)))
+
+	c := client()
+	imageRef := generateImageRef(c, imageTag)
+
+	// Generate the contents of index.html
+	fmt.Printf("Generated index.html = `%s`\n", contents)
+	contentsTarball := tarball(contents)
+
+	// Build + push
+	_ = cmd(fmt.Sprintf("docker build -t %s -", imageRef),
+		withStdin(contentsTarball))
+
+	_ = cmd(fmt.Sprintf("docker push %s", imageRef))
+
+	// Modify the Deployment and apply
+	deployment := appsv1.Deployment{}
+	decodeFile("./deployment.yaml", &deployment)
+
+	deployment.Spec.Template.Spec.Containers[0].Image = imageRef
+
+	if crash {
+		fmt.Println(`[go] Adding command = ["sh", "-c", "exit 1"] because --crash=true`)
+		deployment.Spec.Template.Spec.Containers[0].Command = []string{"sh", "-c", "exit 1"}
+	}
+
+	_ = cmd("kubectl apply -o yaml -f -", withStdin(encode(deployment)))
+
+	color.Green(fmt.Sprintf("[go] kubespy trace %s\n", deployment.Name))
+	kubespy.TraceDeployment("default", deployment.Name)
+}
+
+func sanitize(s string) string {
+	return strings.ToLower(alphaRegexp.ReplaceAllString(s, ""))
+}
+
+func generateImageRef(c *kubernetes.Clientset, tag string) string {
+	registry, _ := localregistry.Discover(context.Background(), c.CoreV1())
+	if registry.Host == "" {
+		fmt.Println("This script requires a cluster with a discoverable registry.\n" +
+			"See https://github.com/tilt-dev/kind-local for an example of how to set this up with Kind")
+		os.Exit(1)
+	}
+
+	imageName := path.Join(registry.Host, "my-busybox")
+	return fmt.Sprintf("%s:%s", imageName, tag)
+}
+
+func prettyAge(pod *v1.Pod) string {
+	t := pod.CreationTimestamp.Time
+	dur := time.Since(t)
+	return fmt.Sprintf("%.3fs", float64(dur)/float64(time.Second))
+}
+
+func tarball(contents string) io.Reader {
+	b := bytes.NewBuffer(nil)
+	w := tar.NewWriter(b)
+	dockerfile := []byte(`
+FROM busybox
+ADD index.html index.html
+ENTRYPOINT busybox httpd -f -p 8000
+`)
+	_ = w.WriteHeader(&tar.Header{
+		Name: "Dockerfile",
+		Mode: 0644,
+		Uid:  0,
+		Gid:  0,
+		Size: int64(len(dockerfile)),
+	})
+
+	_, _ = w.Write(dockerfile)
+
+	_ = w.WriteHeader(&tar.Header{
+		Name: "index.html",
+		Mode: 0644,
+		Uid:  0,
+		Gid:  0,
+		Size: int64(len([]byte(contents))),
+	})
+	_, _ = w.Write([]byte(contents))
+	_ = w.Close()
+	return b
+}
+
+func decodeFile(path string, ptr interface{}) {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	decodeBytes(contents, ptr)
+}
+
+func decodeBytes(b []byte, ptr interface{}) {
+	decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewBuffer(b), 4096)
+	err := decoder.Decode(ptr)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func encode(obj interface{}) io.Reader {
+	jsonData, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := yamlEncoder.JSONToYAML(jsonData)
+	if err != nil {
+		panic(err)
+	}
+	return bytes.NewBuffer(data)
+}
+
+type cmdOption func(*exec.Cmd)
+
+func withStdin(r io.Reader) cmdOption {
+	return func(cmd *exec.Cmd) {
+		cmd.Stdin = r
+	}
+}
+
+func cmd(s string, options ...cmdOption) []byte {
+	fmt.Println(s)
+	cmd := exec.Command("bash", "-c", s)
+	for _, o := range options {
+		o(cmd)
+	}
+	stdout, err := cmd.Output()
+	if err != nil {
+		exitErr, isExitError := err.(*exec.ExitError)
+		if isExitError {
+			fmt.Println("Stderr: ", string(exitErr.Stderr))
+			panic(err)
+		}
+		panic(err)
+	}
+	return stdout
+}
+
+func config() *rest.Config {
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	rules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+
+	overrides := &clientcmd.ConfigOverrides{}
+	loader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides)
+	config, err := loader.ClientConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	return config
+}
+
+func client() *kubernetes.Clientset {
+	clientset, err := kubernetes.NewForConfig(config())
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
+}
+
+func newInformer(cs *kubernetes.Clientset, gvr schema.GroupVersionResource, options ...informers.SharedInformerOption) cache.SharedInformer {
+	factory := informers.NewSharedInformerFactoryWithOptions(cs, 5*time.Second,
+		append([]informers.SharedInformerOption{informers.WithNamespace("default")}, options...)...)
+	resFactory, err := factory.ForResource(gvr)
+	if err != nil {
+		panic(err)
+	}
+
+	return resFactory.Informer()
+}
+
+func runPodInformer(informer cache.SharedInformer, podCallback func(pod *v1.Pod)) {
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			pod, ok := obj.(*v1.Pod)
+			if ok {
+				podCallback(pod)
+			}
+		},
+		UpdateFunc: func(_, obj interface{}) {
+			pod, ok := obj.(*v1.Pod)
+			if ok {
+				podCallback(pod)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			pod, ok := obj.(*v1.Pod)
+			if ok {
+				podCallback(pod)
+			}
+		},
+	})
+	go informer.Run(make(chan struct{}))
+}
+
+type portforwardServer struct {
+	cs        *kubernetes.Clientset
+	pf        *portforward.PortForwarder
+	pfPodName string
+}
+
+func newPortforwardServer(cs *kubernetes.Clientset) *portforwardServer {
+	return &portforwardServer{cs: cs}
+}
+
+func (s *portforwardServer) ConnectToPod(pod *v1.Pod, port int) {
+	if s.pfPodName == pod.Name {
+		return
+	}
+
+	if s.pf != nil {
+		s.pf.Close()
+	}
+
+	color.Green(fmt.Sprintf("Port-forward localhost:%d to %s\n", port, pod.ObjectMeta.Name))
+	transport, upgrader, err := spdy.RoundTripperFor(config())
+	if err != nil {
+		panic(err)
+	}
+
+	req := s.cs.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Namespace(pod.Namespace).
+		Name(pod.Name).
+		SubResource("portforward")
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", req.URL())
+	if err != nil {
+		panic(err)
+	}
+
+	pf, err := portforward.New(dialer, []string{fmt.Sprintf("%d:8000", port)},
+		make(chan struct{}, 1), make(chan struct{}, 1),
+		ioutil.Discard, ioutil.Discard)
+	if err != nil {
+		panic(err)
+	}
+	s.pf = pf
+	s.pfPodName = pod.Name
+
+	go func() {
+		_ = pf.ForwardPorts()
+	}()
+}
--- go.mod
+++ go.mod
@@ -3,18 +3,33 @@ module github.com/tilt-dev/kubecon-2020-talk
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/fatih/color v1.9.0
+	github.com/gofrs/flock v0.8.0 // indirect
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/lib/pq v1.8.0 // indirect
+	github.com/mbrlabs/uilive v0.0.0-20170420192653-e481c8e66f15
 	github.com/pkg/errors v0.9.1
+	github.com/pulumi/kubespy v0.6.0
+	github.com/pulumi/pulumi-kubernetes v1.6.0
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/cobra v1.0.0
 	github.com/tilt-dev/localregistry-go v0.0.0-20200629152853-95301f6df442
 	github.com/tjarratt/babble v0.0.0-20191209142150-eecdf8c2339d
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
-	helm.sh/helm/v3 v3.1.0-rc.1.0.20201005141519-fc9b46067f8f
-	k8s.io/api v0.19.2
-	k8s.io/apimachinery v0.19.2
-	k8s.io/cli-runtime v0.19.2
-	k8s.io/client-go v0.19.2
+	helm.sh/helm/v3 v3.3.3
+	k8s.io/api v0.18.8
+	k8s.io/apiextensions-apiserver v0.18.8 // indirect
+	k8s.io/apimachinery v0.18.8
+	k8s.io/cli-runtime v0.18.8
+	k8s.io/client-go v0.18.8
 	k8s.io/klog v1.0.0 // indirect
-	k8s.io/kubectl v0.19.2
+	k8s.io/kubectl v0.18.8
 	sigs.k8s.io/yaml v1.2.0
+)
+
+replace (
+	github.com/Azure/go-autorest => github.com/Azure/go-autorest v13.3.1+incompatible
+	github.com/evanphx/json-patch => github.com/evanphx/json-patch v0.0.0-20200808040245-162e5629780b // 162e5629780b is the SHA for git tag v4.8.0
 )
